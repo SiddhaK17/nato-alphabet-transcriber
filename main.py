ba663581139be831808fd54d2b552df80c654695
Siddha Kadam <siddhakad17@gmail.com>
2026-06-03 00:20:33 +0530

    Improve input handling in generate_phonetic function
    Refactor generate_phonetic function to handle empty input and improve user prompts.

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
+# print(phonetic_dict)    -> Prints out the dictionary therefore usefull for debugging
 
 
 def generate_phonetic():
-    word = input("Enter a word: ").upper()
-    try:
-        output_list = [phonetic_dict[letter] for letter in word]
-    except KeyError:
-        print("Sorry, only letters in the alphabets please.")
-        generate_phonetic()
-    else:
-        print(output_list)
+    while True:
+        word = input("Enter a word: ").strip().upper()
+
+        if not word:
+            print("Please enter a word.")
+            continue
+
+        try:
+            output_list = [phonetic_dict[letter] for letter in word]
+
+        except KeyError:
+            print("Sorry, only letters in the alphabet please.")
+
+        else:
+            print(output_list)
+            break
 
 generate_phonetic()
